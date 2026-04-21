5d18563b8718e3a949666812083a75328810929d
graeeez <gepastoral.student@ua.edu.ph>
2026-04-21 14:34:07 +0800

    updated song controller and pom.xml

--- src/main/java/com/pastoral/song/SongController.java
+++ src/main/java/com/pastoral/song/SongController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/pastoral/songs")
 public class SongController {
 
